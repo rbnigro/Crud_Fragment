2475ce97fbf7bfc99fbb7fc594ebc662659330d8
Ronney Nigro <ronneynigro@gmail.com>
2020-08-28 12:59:09 -0300

    First Commit

---
+++ app/src/main/java/com/ronney/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -0,0 +1,20 @@
+package com.ronney.mysubscribers.ui.subscriber
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ronney.mysubscribers.R
+
+class SubscriberFragment : Fragment() {
+
+    private lateinit var viewModel: SubscriberViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/ronney/mysubscribers/ui/subscriber/SubscriberViewModel.kt
@@ -0,0 +1,7 @@
+package com.ronney.mysubscribers.ui.subscriber
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
