238873c1ff10e68bb290ee4a92afd7a5b01f0018
Ronney Nigro <ronneynigro@gmail.com>
2020-08-31 12:58:58 -0300

    Apanhando do Layout

--- app/src/main/java/com/ronney/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/ronney/mysubscribers/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ronney.mysubscribers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        var navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        app_toolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
