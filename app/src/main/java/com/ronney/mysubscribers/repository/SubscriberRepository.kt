1a94da716edd7dafe3dbe359d91145e8b498cc50
Ronney Nigro <ronneynigro@gmail.com>
2020-09-08 15:13:41 -0300

    Criação das camadas de CRUD (v2)

--- app/src/main/java/com/ronney/mysubscribers/repository/SubscriberRepository.kt
+++ app/src/main/java/com/ronney/mysubscribers/repository/SubscriberRepository.kt
@@ -6,6 +6,7 @@ import com.ronney.mysubscribers.data.db.entity.SubscriberEntity
 
 interface SubscriberRepository {
 
+    // suspend fun, pode ser pausada e e retomada posteriormente
     suspend fun  insertSubscriber(name: String, email: String): Long
     suspend fun updateSubscriber(id: Long, name: String, email: String)
     suspend fun deleteSubscriber(id: Long)
